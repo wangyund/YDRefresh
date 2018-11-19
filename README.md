2f3918b584226085d982654c111056cc13d61b82
wyd <wyd@rowenatech.com>
2018-11-19 17:47:26 +0800

    更新md文件.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# YDRefresh
 允顶ObjectC版刷新库
+
+Pod添加方式：
+
+pod 'YDRefresh' ,:git => 'https://github.com/wangyund/YDRefresh.git'
+
--- YDRefresh/Class/AppDelegate/AppDelegate.h
+++ YDRefresh/Class/AppDelegate/AppDelegate.h
@@ -5,6 +5,10 @@
 //  Created by rowena on 2018/11/19.
 //  Copyright © 2018年 rowena. All rights reserved.
 //
+/**
+ * 添加pod方式
+ */
+//
 
 #import <UIKit/UIKit.h>
 
